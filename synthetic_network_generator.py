b8f407731919b9c5436b2f51b71c7eb0945037bd
JohanWu <tsunghanwu511@gmail.com>
2018-05-16 16:12:42 +0800

    Add varying step migration function for Stochastic Block Model

--- synthetic_network_generator.py
+++ synthetic_network_generator.py
@@ -1,17 +1,16 @@
 '''Program for generating synthetic network sequence'''
 
 # Author: Tsunghan Wu <tsunghanwu511@gmail.com>
-# Last Update: 2018/04/26
+# Last Update: 2018/05/16
 # Usage: 
 #	python synthetic_network_generator.py
-#	network generation model: {'sbm_one_step', 'sw_sf_one_step'}
 
 
 import networkx as nx
 import random
 import pickle
 
-## Parametes for network generation
+## Parametes for SBM network generation
 NUM_NODES = 100
 NUM_CLUSTERS = 4
 P_IN = 0.8
@@ -20,8 +19,8 @@ NUM_MOVING_NODES = 20	# usually 2*NUM_CLUSTERS
 FIRST_MOVING_NODE = 1	# ID of the first moving node
 TIME_PERIOD = 1000
 
-class SyntheticNetwork(object):
-	'''For generating syhthetic network sequence.
+class SyntheticNetwork_SBM(object):
+	'''For generating synthetic network sequence by Stochastic Block Model (SBM).
 
 	Parameters:
 	-----------
@@ -82,7 +81,7 @@ class SyntheticNetwork(object):
 	        if node_id < 0: return i
 
 	def sbm_one_step(self):
-		'''Build the network by Stochastic Block Model.
+		'''Build the network by Stochastic Block Model. Each moving node migrates to the next community at each time.
 
 		'''
 		### generate the first network
@@ -121,31 +120,75 @@ class SyntheticNetwork(object):
 			network_sequence.append(network.copy())
 			# self.check_links(network_sequence[-1])       
 		return network_sequence
-    
 
-	def sw_sf_one_step():
-		'''Build the networks which have the community structures with Small World and Scale Free properties.
+	def sbm_varying_step(self):
+		'''Build the network by Stochastic Block Model. Each moving node migrates to the community following the rule: j' = ((j + (t mod i) + 1) mod K). j': next community, j: current community, t: time, i: node's ID, K: number of clusters.
 
 		'''
-		pass
+		### generate the first network
+		network = nx.Graph()
+		## create nodes
+		for i in range(self.num_nodes):
+			network.add_node(i, comm=self._get_community(i))
+		## build links
+		for i in range(self.num_nodes):
+			for j in range(i+1, self.num_nodes, 1):
+				if network.node[i]['comm'] == network.node[j]['comm']: # in the same block
+					if random.random() < self.p_in: network.add_edge(i, j)
+				else:
+					if random.random() < self.p_out: network.add_edge(i, j)
+    	## check links
+		self.check_links(network)
+    	
+		network_sequence = list([network.copy()])
+		### Simple one-step migration
+            
+		for t in range(1, self.time_period):
+			## decide the next communities for moving nodes
+			next_comm = [(network.node[n]['comm'] + (t % n) + 1) % self.num_clusters for n in self.moving_nodes]
+			## rewire the links for migrating nodes
+			network.remove_nodes_from(self.moving_nodes)
+			for i, n in enumerate(self.moving_nodes):
+				network.add_node(n, comm=next_comm[i])
+			for n in self.moving_nodes:
+				for j in network.nodes():
+					if n != j:
+					    if network.node[n]['comm'] == network.node[j]['comm']: # in the same block
+					        if random.random() < self.p_in: network.add_edge(n, j)
+					    else:
+					        if random.random() < self.p_out: network.add_edge(n, j)
+			# self.check_links(network)
+			network_sequence.append(network.copy())
+			# self.check_links(network_sequence[-1])       
+		return network_sequence
+    
+class SyntheticNetwork_SocialNetwork(object):
+	'''For generating synthetic network sequence. The networks has community structures with small world and scale free properties which social networks have.
+
+	'''
 
 if __name__ == "__main__":
-	# model = input("Choose network generation model:\n[1]sbm_one_step\n[2]sw_sf_one_step\n:")
+	# model = input("Choose network generation model:\n[1]SBM\n[2]Social Network\n:")
 	model = '1'
 
-	## Network Initialization
-	network = SyntheticNetwork(NUM_NODES, NUM_CLUSTERS, P_IN, P_OUT, NUM_MOVING_NODES, FIRST_MOVING_NODE, TIME_PERIOD)
-	# print(network.moving_nodes)
-	# print(network.num_nodes_in_community)
-
 	if model == '1':
-		print('Do sbm_one_step')
 		## generate a SBM-based network sequence, return a list of networks
-		network_sequence = network.sbm_one_step()
+		print('Synthetic Network Generation by SBM')
+
+		## Network Initialization
+		network = SyntheticNetwork_SBM(NUM_NODES, NUM_CLUSTERS, P_IN, P_OUT, NUM_MOVING_NODES, FIRST_MOVING_NODE, TIME_PERIOD)
+		# print(network.moving_nodes)
+		# print(network.num_nodes_in_community)
+
+		# network_sequence = network.sbm_one_step()
+		network_sequence = network.sbm_varying_step()
 		
 	elif model == '2':
-		## generate a network sequence with Small World and Scale-free properties, return a list of networks
-		network_sequence = network.sw_sf_one_step()
+		## generate a network sequence which has community structure with Small World and Scale-free properties, return a list of networks
+		print('Social Network Generation')
+
+		network = SyntheticNetwork_SocialNetwork()
+
 
 	# print(len(network_sequence))
 	## Write the network sequence into file for reuse.
