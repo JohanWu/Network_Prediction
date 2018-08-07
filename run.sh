05f60fa4952b3256cf54b8de31e1fa96fc33376d
JohanWu <tsunghanwu511@gmail.com>
2018-08-07 09:57:31 +0800

    for comparing RNN models with different activation functions

--- run.sh
+++ run.sh
@@ -10,9 +10,6 @@ do
 rm -f *.p
 python synthetic_network_generator.py
 python main.py 'linear'
-python main.py 'sigmoid'
-python main.py 'tanh'
-python main.py 'elu'
 python main.py 'selu'
 python main.py 'relu'
 done
